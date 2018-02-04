d57a731f4b3b2160c1b147ce5511dcda21ab45df
YxMingY <526817087@qq.com>
2018-02-05 00:53:43 +0800

    房间模块现在不会报错啦233

--- src/WildernessBattle/Modules/room/RoomManager.php
+++ src/WildernessBattle/Modules/room/RoomManager.php
@@ -1,3 +1,14 @@
 <?php
 namespace WildernessBattle\Modules\room;
+
 use WildernessBattle\Main;
+use WildernessBattle\abstractc\Module;
+
+class RoomManager extends Module{
+   public $name = "房间模块";
+   protected $listeners = [];
+   public function enable(){
+      $this->registerListeners();
+      parent::enable();
+   }
+}
